dafc79e226a1b5230d59bccbd063ac98b3278379
Маньковський Олександр <117378642+RootSasha@users.noreply.github.com>
2024-11-24 14:34:25 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,19 +12,5 @@ GRAFANA_USER="admin"
 GRAFANA_PASSWORD="admin"
 GRAFANA_API_URL="http://localhost:3000/api/dashboards/db"
 
-import_dashboard() {
-  local dashboard_id=$1
-  local dashboard_file="$dashboard_id.json"
-  
-  curl -X POST \
-       -u "$GRAFANA_USER:$GRAFANA_PASSWORD" \
-       -H "Content-Type: application/json" \
-       --data-binary "@$dashboard_file" \
-       "$GRAFANA_API_URL"
-}
-
-import_dashboard 14857
-import_dashboard 18699
-import_dashboard 6593
 
 echo "Dashboards imported."
